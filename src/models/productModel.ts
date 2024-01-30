2f6c214394f65b28d883d0f939d1d9f7d3fe325a
Mateusz MG <MateuszMG.default@com.com>
2024-01-30 07:59:24 +0100

    fix(PAB-10): replace Array to Map in productModel

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -7,52 +7,62 @@ interface Product {
   category: string;
 }
 
-const dbProducts: Product[] = [
-  {
-    category: 'laptops',
-    id: '1',
-    name: 'Laptop 1',
-    price: 456,
-    quantity: 2,
-    productionDate: '2015-12-12',
-  },
-  {
-    category: 'laptops',
-    id: '2',
-    name: 'Laptop 3',
-    price: 1947,
-    quantity: 20000,
-    productionDate: '2015-11-11',
-  },
-  {
-    category: 'laptops',
-    id: '3',
-    name: 'Laptop 3',
-    price: 2680,
-    quantity: 8,
-    productionDate: '2015-10-10',
-  },
-];
+const dbProducts: Map<string, Product> = new Map([
+  [
+    '1',
+    {
+      category: 'laptops',
+      id: '1',
+      name: 'Laptop 1',
+      price: 456,
+      quantity: 2,
+      productionDate: '2015-12-12',
+    },
+  ],
+  [
+    '2',
+    {
+      category: 'laptops',
+      id: '2',
+      name: 'Laptop 2',
+      price: 1947,
+      quantity: 20000,
+      productionDate: '2015-11-11',
+    },
+  ],
+  [
+    '3',
+    {
+      category: 'laptops',
+      id: '3',
+      name: 'Laptop 3',
+      price: 2680,
+      quantity: 8,
+      productionDate: '2015-10-10',
+    },
+  ],
+]);
 
 export const productModel = {
   getById: (id: string) => {
-    return dbProducts.find((product) => product.id === id);
+    return dbProducts.get(id);
   },
 
   getMany: () => {
-    return dbProducts;
+    return Array.from(dbProducts.values());
   },
 
   create: (newProduct: Omit<Product, 'id'>) => {
     const product: Product = { ...newProduct, id: Date.now().toString() };
-    dbProducts.push(product);
+    dbProducts.set(product.id, product);
     return product;
   },
 
-  update: (upatedProduct: Product) => {
-    dbProducts.map((product) =>
-      product.id === upatedProduct.id ? upatedProduct : product,
-    );
-    return upatedProduct;
+  update: (updatedProduct: Product) => {
+    if (dbProducts.has(updatedProduct.id)) {
+      dbProducts.set(updatedProduct.id, updatedProduct);
+    }
+
+    return updatedProduct;
   },
 };
